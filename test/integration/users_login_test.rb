da68d10a7c1d4b3628b6c10b076d17aaff0eb5f1
AgustinPagotto <agustinpagotto@gmail.com>
2024-09-26 16:40:36 -0300

    deploy script

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,57 +1,12 @@
 require 'test_helper'
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
+end
 
-  test 'login with valid information followed by logout' do
-    post login_path, params: { session: { email: @user.email, password: 'password' } }
-    assert is_logged_in?
-    assert_redirected_to @user
-    follow_redirect!
-    assert_template 'users/show'
-    # Verifies that the login path anchor is not present
-    assert_select 'a[href=?]', login_path, count: 0
-    assert_select 'a[href=?]', logout_path
-    assert_select 'a[href=?]', user_path(@user)
-    delete logout_path
-    assert_not is_logged_in?
-    assert_response :see_other
-    assert_redirected_to root_url
-    # simulate a user clicking logout in a second window
-    delete logout_path
-    follow_redirect!
-    assert_select 'a[href=?]', login_path
-    assert_select 'a[href=?]', logout_path, count: 0
-    assert_select 'a[href=?]', user_path(@user), count: 0
-  end
-  test 'invalid login information' do
-    get login_path
-    assert_template 'session/new'
-    post login_path, params: {
-      session: { email: 'user@invalid',
-                 password: 'foo' }
-    }
-    assert_response :unprocessable_entity
-    assert_template 'session/new'
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
-  end
-  test 'login with remembering' do
-    log_in_as(@user)
-    # Blank is because the cookie can respond with ''
-    assert_not cookies[:remember_token].blank?
-  end
-  test 'login without remembering' do
-    # Log in to set cookie
-    log_in_as(@user)
-    # Log in again and verify if the cookie is deleted
-    log_in_as(@user, remember_me: '0')
-    assert cookies[:remember_token].blank?
-  end
-  # Exercise
+class InvalidPasswordTest < UsersLogin
   test 'login with valid email/invalid password' do
     get login_path
     assert_template 'session/new'
@@ -60,10 +15,70 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
                  password: 'foo' }
     }
     assert_not is_logged_in?
-    assert_response :unprocessable_entity
     assert_template 'session/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+end
+
+class ValidLogin < UsersLogin
+  def setup
+    super
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+  end
+end
+
+class ValidLoginTest < ValidLogin
+  test 'valid login' do
+    assert is_logged_in?
+    assert_redirected_to @user
+  end
+  test 'redirect after login' do
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
+  end
+end
+
+class Logout < ValidLogin
+  def setup
+    super
+    delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+  test 'successful logout' do
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
+  test 'redirect after logout' do
+    follow_redirect!
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
+  end
+  test 'should still work after logout in second window' do
+    delete logout_path
+    assert_redirected_to root_url
+  end
+end
+
+class RememberingTest < UsersLogin
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+  end
+  test 'login without remembering' do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
   end
 end
